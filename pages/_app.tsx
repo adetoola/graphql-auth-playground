b15a9a0ef48c5181a618ee9fb6bc394add0332de
Adetola Onasanya <adetola.onasanya@gmail.com>
2021-10-16 17:44:50 +0100

    Add apollo-client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ApolloProvider } from '@apollo/client';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useApollo } from '../lib/apolloClient';
+
+function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default App;
